7b6a5ae39cb5beda596b4964fa6892927b74df77
MSinkova <msinkova92@gmail.com>
2024-04-12 20:50:32 +0300

    1.исправлен метод Arrays.fill 2.убраны избыточные проверки в методах 3.исправлен метод size() 4.добавлена проверка строки через equals() 5.изменена реализация метода getAll()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,37 +8,30 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size - 1, null);
     }
 
     void save(Resume r) {
-        if(storage[size] == null) {
-            storage[size] = r;
-            size++;
-            return;
-        }
-        System.out.println("ERROR: Rezume already exists");
+        storage[size] = r;
+        size++;
+        return;
     }
 
     Resume get(String uuid) {
-        Resume[] storageWithoutNull = getAll();
-        for(int i = 0; i< storageWithoutNull.length; i++) {
-           if(storageWithoutNull[i].toString() == uuid) {
-               return storageWithoutNull[i];
-           }
-
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                return storage[i];
+            }
         }
         return null;
     }
 
     void delete(String uuid) {
-        for(int i = 0; i < size; i++) {
-            if(storage[i] != null) {
-               if(uuid == storage[i].toString()) {
-                   storage[i] = storage[size - 1];
-                   storage[size - 1] = null;
-                   size--;
-               }
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
         }
     }
@@ -48,20 +41,16 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] storageWithoutNull = new Resume[size()];
-        for(int i = 0; i < storage.length; i++) {
-            if(storage[i] != null) {
-                storageWithoutNull[i] = storage[i];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storageWithoutNull = Arrays.copyOf(storage, size);
             }
+            break;
         }
         return storageWithoutNull;
     }
 
     int size() {
-        for(size = 0; size < storage.length; size++) {
-           if (storage[size] != null) {
-               size++;
-           }
-        }
         return size;
     }
 }
